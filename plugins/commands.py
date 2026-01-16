c8a762878104a101f7e210e3941f17168cd8d617
suryabm46 <suryachandan701@gmail.com>
2026-01-17 00:01:33 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -84,7 +84,7 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('❤️ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ❤️', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('🍁 Update Channel 🍁', url=UPDATE_CHNL_LNK)
+                    InlineKeyboardButton('🍁 Update Channel 🍁', url='https://t.me/SB_KANNADA_MOVE_WORLD')
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.GSTART_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
