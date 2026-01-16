9ba988bb345ff01d65850246197c8951db2e2c18
suryabm46 <suryachandan701@gmail.com>
2026-01-16 23:57:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,9 +120,9 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/technokrrish') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/technokrrish') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/technokrrish') # Update channel link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/SB_KANNADA_MOVE_WORLD') # Group link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/suryabm04) # Owner link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/SB_KANNADA_MOVE_WORLD') # Update channel link for the bot
 
 # ============================
 # User Configuration
